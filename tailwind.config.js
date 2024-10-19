584e269c20b01b3f686f276d76f086b0998e07d4
redreev8 <redreevs@mail.ru>
2024-10-20 03:23:40 +0500

    talwind: add fonts Jura & animate shadow-drop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				Jura: ['Jura', 'sans-serif'],
+			},
+			animation: {
+				'shadow-drop': 'shadow-drop .6s linear both',
+			},
+		},
 	},
 	plugins: [],
 }
