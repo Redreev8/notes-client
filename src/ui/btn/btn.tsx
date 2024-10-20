30a699e0c40bcc0cd3b780b3045d89ab1ace0daa
redreev8 <redreevs@mail.ru>
2024-10-20 19:59:39 +0500

    UI: tag a to Link react router && color text neutral to slate

--- src/ui/btn/btn.tsx
+++ src/ui/btn/btn.tsx
@@ -5,6 +5,7 @@ import {
 	AnchorHTMLAttributes,
 } from 'react'
 import BtnContent, { BtnContentProps } from './btn-content'
+import { Link } from 'react-router-dom'
 
 export interface Props extends BtnContentProps {
 	href?: string
@@ -27,22 +28,22 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(function ButtonRef(
 	)
 })
 const A = forwardRef<HTMLAnchorElement, LinkProps>(function ARef(
-	{ iconLeft, iconRight, children, ...props },
+	{ iconLeft, href, iconRight, children, ...props },
 	ref,
 ) {
 	return (
-		<a ref={ref} {...props}>
+		<Link to={href!} ref={ref} {...props}>
 			<BtnContent iconLeft={iconLeft} iconRight={iconRight}>
 				{children}
 			</BtnContent>
-		</a>
+		</Link>
 	)
 })
 
 const Btn = forwardRef<HTMLButtonElement | HTMLAnchorElement, LinkProps | ButtonProps>(
 	function BtnRef({ className, children, href, isFilled, ...props }, ref) {
 		const cl =
-			'font-Jura text-slate-950 text-xl bg-slate-50 px-3 py-1 rounded-full transition-opacity delay-150 hover:opacity-75 hover:animate-shadow-drop'
+			'font-Jura text-slate-950 text-xl bg-neutral-50 px-3 py-1 rounded-full transition-opacity delay-150 hover:opacity-75 hover:animate-shadow-drop'
 		if (href) {
 			return (
 				<A
