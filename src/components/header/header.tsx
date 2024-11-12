e695a630b1f6b8707c3f01c2c0af61470cca0643
redreev8 <redreevs@mail.ru>
2024-11-12 16:41:58 +0500

    ui: gray to slate

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,10 +5,10 @@ import { selectHeaderItems } from '../../store/header-items.slice'
 const Header: FC = () => {
 	const { left, center, right } = useSelector(selectHeaderItems)
 	return (
-		<header className="fixed bottom-0 left-0 z-40 w-full overflow-auto border-t-2 border-gray-700 bg-gray-950">
-			<div className="inline-flex min-w-full gap-1 border-gray-700 p-2 lg:px-6">
+		<header className="fixed bottom-0 left-0 z-40 w-full overflow-auto border-t-2 border-slate-700 bg-slate-950">
+			<div className="inline-flex min-w-full gap-1 border-slate-700 p-2 lg:px-6">
 				<div className="flex flex-1 gap-1 justify-self-start">
-					<Btn className="uppercase" isSamll>
+					<Btn className="uppercase" isSmall>
 						меню
 					</Btn>
 					{left && (
